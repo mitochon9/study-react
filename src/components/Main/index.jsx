badb35d5751a4893e1aba8cf3bc3287552a47b7c
mitochon9-macbook <79145642+mitochon9@users.noreply.github.com>
2021-05-22 00:03:00 +0900

    🏗️ 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
-import classes from "./Main.module.css";
-import { Links } from "./Links";
-import { Headline } from "./Headline";
+import classes from "src/components/Main/Main.module.css";
+import { Headline } from "src/components/Headline";
+import { Links } from "src/components/Links";
 
 export function Main(props) {
   return (
